bf820f3cbcd8e89bcb045e150d5543413fac0aa2
Devin <devincorrigall@gmail.com>
2017-03-04 12:20:10 -0800

    Add Astar and food hunting

--- app/main.py
+++ app/main.py
@@ -45,7 +45,7 @@ def move():
 
 def get_move(data):
     groot = get_groot(data)
-    return default(data)
+    return hungry(data)
     # if groot["health"] < 25:
     #     return hungry(data)
     # else:
@@ -61,6 +61,8 @@ def default(data):
     groot = get_groot(data)
 
     map = make_map(data)
+
+
 
     return "down"
 
@@ -106,21 +108,53 @@ def default(data):
 #     if west == minimum:
 #         return [coord[0]-1,coord[1]]
 
-# def hungry(data):
+def hungry(data):
 
-#     groot = get_groot(data)
-#     map = make_map(data)
+    groot = get_groot(data)
+    map = make_map(data)
 
-#     if not len(data["food"]):
-#         return default(data)
+    if not len(data["food"]):
+        return default(data)
 
-#     food = food_eval(map, data["food"], shia["coords"][0])
+    food = food_eval(map, data["food"], groot["coords"][0])
 
-#     if not len(food):
-#         return default(data)
+    if not len(food):
+        return default(data)
 
-#     return get_move(shia["coords"][0], food, data)
+    return get_move(groot["coords"][0], food, data)
 
+
+def food_eval(map, data_food, our_head):
+        food_distance = []
+        for food in data_food:
+            food_distance.append(get_distance(our_head, food))
+        sorted(food_distance)
+        for food in food_distance:
+            print food
+            if(is_food_safe(food[1], 1, map)):
+                return food[1]
+        for food in food_distance:
+            print food
+            if(is_food_safe(food[1], 2, map)):
+                return food[1]
+        for food in food_distance:
+            print food
+            if(is_food_safe(food[1], 3, heat_map)):
+                return food[1]
+        return []
+
+
+def get_distance(our_head, food_coords):
+    x_distance = abs(our_head[0] - food_coords[0])
+    y_distance = abs(our_head[1] - food_coords[1])
+    return [ x_distance + y_distance , food_coords]
+
+
+def is_food_safe(food_coords, threshold, heat_map):
+    #TODO: ADD TO THIS!!!!!!!!!!!!!!!!!!!!!
+
+
+    return heat_map[food_coords[0]][food_coords[1]] <= threshold
 
 
 def make_map(data):
@@ -145,7 +179,6 @@ def make_map(data):
     for food in data["food"]:
         wall_coords.append(food)
 
-
     for wall in wall_coords:
         y = wall[0]
         x = wall[1]
@@ -163,6 +196,207 @@ def make_map(data):
     print(map)
 
     return map
+
+
+
+
+def get_move(start, goal, data):
+    wall_coords     = []
+    start           = tuple(start)
+    goal            = tuple(goal)
+
+    for snake in data["snakes"]:
+        if snake["id"] == data['you']:
+            for body in snake["coords"][1:]:
+                wall_coords.append(tuple(body))
+        else:
+            for body in snake["coords"]:
+                wall_coords.append(tuple(body))
+
+    for food in data["food"]:
+        food.append(tuple(food))
+
+    print "WALL COORDS"
+    print wall_coords
+
+    a = AStar()
+
+    a.init_grid(data["height"],data["width"],wall_coords,start,goal)
+
+    solution = a.solve()
+
+    print solution
+
+    if solution:
+        return convert_direction(start, solution[1])
+
+    return None
+
+
+
+def convert_direction(start, coord):
+
+    if start[0] > coord[0]:
+        print "left"
+        return "left"
+    elif start[0] < coord[0]:
+        print "right"
+        return "right"
+
+    if start[1] > coord[1]:
+        print "up"
+        return "up"
+
+    print "down"
+    return "down"
+
+
+
+
+
+'''
+Thanks to Laurent Luce for supplying A*
+https://github.com/laurentluce/python-algorithms/
+'''
+
+class Cell(object):
+    def __init__(self, x, y, reachable):
+        """Initialize new cell.
+        @param reachable is cell reachable? not a wall?
+        @param x cell x coordinate
+        @param y cell y coordinate
+        @param g cost to move from the starting cell to this cell.
+        @param h estimation of the cost to move from this cell
+                 to the ending cell.
+        @param f f = g + h
+        """
+        self.reachable = reachable
+        self.x = x
+        self.y = y
+        self.parent = None
+        self.g = 0
+        self.h = 0
+        self.f = 0
+
+
+
+class AStar(object):
+    def __init__(self):
+        # open list
+        self.opened = []
+        heapq.heapify(self.opened)
+        # visited cells list
+        self.closed = set()
+        # grid cells
+        self.cells = []
+        self.grid_height = None
+        self.grid_width = None
+
+    def init_grid(self, width, height, walls, start, end):
+        """Prepare grid cells, walls.
+        @param width grid's width.
+        @param height grid's height.
+        @param walls list of wall x,y tuples.
+        @param start grid starting point x,y tuple.
+        @param end grid ending point x,y tuple.
+        """
+        self.grid_height = height
+        self.grid_width = width
+        for x in range(self.grid_width):
+            for y in range(self.grid_height):
+                if (x, y) in walls:
+                    reachable = False
+                else:
+                    reachable = True
+                self.cells.append(Cell(x, y, reachable))
+        self.start = self.get_cell(*start)
+        self.end = self.get_cell(*end)
+
+    def get_heuristic(self, cell):
+        """Compute the heuristic value H for a cell.
+        Distance between this cell and the ending cell multiply by 10.
+        @returns heuristic value H
+        """
+        return 10 * (abs(cell.x - self.end.x) + abs(cell.y - self.end.y))
+
+    def get_cell(self, x, y):
+        """Returns a cell from the cells list.
+        @param x cell x coordinate
+        @param y cell y coordinate
+        @returns cell
+        """
+        return self.cells[x * self.grid_height + y]
+
+    def get_adjacent_cells(self, cell):
+        """Returns adjacent cells to a cell.
+        Clockwise starting from the one on the right.
+        @param cell get adjacent cells for this cell
+        @returns adjacent cells list.
+        """
+        cells = []
+        if cell.x < self.grid_width-1:
+            cells.append(self.get_cell(cell.x+1, cell.y))
+        if cell.y > 0:
+            cells.append(self.get_cell(cell.x, cell.y-1))
+        if cell.x > 0:
+            cells.append(self.get_cell(cell.x-1, cell.y))
+        if cell.y < self.grid_height-1:
+            cells.append(self.get_cell(cell.x, cell.y+1))
+        return cells
+
+    def get_path(self):
+        cell = self.end
+        path = [(cell.x, cell.y)]
+        while cell.parent is not self.start:
+            cell = cell.parent
+            path.append((cell.x, cell.y))
+
+        path.append((self.start.x, self.start.y))
+        path.reverse()
+        return path
+
+    def update_cell(self, adj, cell):
+        """Update adjacent cell.
+        @param adj adjacent cell to current cell
+        @param cell current cell being processed
+        """
+        adj.g = cell.g + 10
+        adj.h = self.get_heuristic(adj)
+        adj.parent = cell
+        adj.f = adj.h + adj.g
+
+    def solve(self):
+        """Solve maze, find path to ending cell.
+        @returns path or None if not found.
+        """
+        # add starting cell to open heap queue
+        heapq.heappush(self.opened, (self.start.f, self.start))
+        while len(self.opened):
+            # pop cell from heap queue
+            f, cell = heapq.heappop(self.opened)
+            # add cell to closed list so we don't process it twice
+            self.closed.add(cell)
+            # if ending cell, return found path
+            if cell is self.end:
+                return self.get_path()
+            # get adjacent cells for cell
+            adj_cells = self.get_adjacent_cells(cell)
+            for adj_cell in adj_cells:
+                if adj_cell.reachable and adj_cell not in self.closed:
+                    if (adj_cell.f, adj_cell) in self.opened:
+                        # if adj cell in open list, check if current path is
+                        # better than the one previously found
+                        # for this adj cell.
+                        if adj_cell.g > cell.g + 10:
+                            self.update_cell(adj_cell, cell)
+                    else:
+                        self.update_cell(adj_cell, cell)
+                        # add adj cell to open list
+                        heapq.heappush(self.opened, (adj_cell.f, adj_cell))
+
+
+
+
 
 
 
