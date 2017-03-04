93b774c0463edaf8ef03ddc4c3546fef3f026404
Devin <devincorrigall@gmail.com>
2017-03-04 11:49:31 -0800

    copied in some stuff from last year

--- app/main.py
+++ app/main.py
@@ -34,10 +34,119 @@ def move():
     # TODO: Do things with data
     directions = ['up', 'down', 'left', 'right']
 
+
+    move = get_move(data)
+
     return {
-        'move': random.choice(directions),
+        'move': move,
         'taunt': 'I am Groot!'
     }
+
+
+def get_move(data):
+    groot = get_groot(data)
+    return default(data)
+    # if groot["health"] < 25:
+    #     return hungry(data)
+    # else:
+    #     return default(data)
+
+def get_groot(data):
+    for snake in data["snakes"]:
+        if snake["id"] == data["you"]:
+            return snake
+
+
+def default(data):
+    groot = get_groot(data)
+
+    map = make_map(data)
+
+    return "down"
+
+
+
+
+# def evaluate(coord, map):
+#     value = 0
+
+#     if coord[1] == 0:
+#         north = 100
+#     else:
+#         north = map[coord[1]-1][coord[0]]
+
+#     if coord[1] == (len(map)-1):
+#         south = 100
+#     else:
+#         south = heatMap[coord[1]+1][coord[0]]
+
+#     if coord[0] == (len(heatMap[0])-1):
+#         east = 100
+#     else:
+#         east = heatMap[coord[1]][coord[0]+1]
+
+#     if coord[0] == 0:
+#         west = 100
+#     else:
+#         west = heatMap[coord[1]][coord[0]-1]
+
+#     values = [north, south, east, west]
+
+#     minimum = min(values)
+
+#     if north == minimum:
+#         return [coord[0],coord[1]-1]
+
+#     if south == minimum:
+#         return [coord[0],coord[1]+1]
+
+#     if east == minimum:
+#         return [coord[0]+1,coord[1]]
+
+#     if west == minimum:
+#         return [coord[0]-1,coord[1]]
+
+# def hungry(data):
+
+#     groot = get_groot(data)
+#     map = make_map(data)
+
+#     if not len(data["food"]):
+#         return default(data)
+
+#     food = food_eval(map, data["food"], shia["coords"][0])
+
+#     if not len(food):
+#         return default(data)
+
+#     return get_move(shia["coords"][0], food, data)
+
+
+
+def make_map(data):
+    wall_coords = []
+    map = []
+
+    for y in range(data["height"]):
+        row = []
+        for j in range(data["width"]):
+            row.append(0)
+        map.append(row)
+
+
+    for snake in data["snakes"]:
+        if snake["id"] == data["you"]:
+            for body in snake["coords"][1:]:
+                wall_coords.append(body)
+        else:
+            for body in snake["coords"]:
+                wall_coords.append(body)
+    print(map)
+
+    return heatMap
+
+
+
 
 
 # Expose WSGI app (so gunicorn can find it)
