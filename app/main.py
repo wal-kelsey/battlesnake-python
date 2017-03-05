8b8983ff38aecf6455de95681dc1219b200ddc05
Devin <devincorrigall@gmail.com>
2017-03-04 16:15:49 -0800

    increase health threshold

--- app/main.py
+++ app/main.py
@@ -81,7 +81,7 @@ def move():
 
 def get_move(data):
     groot = get_groot(data)
-    if groot["health_points"] < 25:
+    if groot["health_points"] < 30:
         return hungry(data)
     else:
         return default(data)
